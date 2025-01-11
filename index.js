69b313d8c9b570e3e514aa2a1ab19c909d10c3ed
tarekbinjaman <tarekbinjaman@gmail.com>
2025-01-12 02:40:13 +0600

    env file updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const uri = "mongodb+srv://job-matcher76:fD91txjfnQgmyWQo@cluster0.vkwnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vkwnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
